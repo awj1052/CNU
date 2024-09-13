27cd13ab1569a616cc39ac71b158c63d9caa2c19
awj1052 <awj1052@naver.com>
2024-09-13 21:28:59 +0900

    remove: file

--- 2학년/자료구조/1주차/src/MainJava.java
+++
@@ -1,9 +0,0 @@
-public class MainJava {
-
-	public static void main(String[] args) {
-		InputManager inputManager = new InputManager();
-		int[] inputs = inputManager.getContents("input.txt");
-		Assignment assignment = new Assignment(inputs);
-		assignment.print_pathA(0);
-	}
-}
